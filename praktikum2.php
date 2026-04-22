3d2146f05745cccad5641e388fa2e8cd5ac42670
ryvel2344 <i43252645@student.polije.ac.id>
2026-04-22 21:41:31 +0700

    Add files via upload

---
+++ praktikum2.php
@@ -0,0 +1,19 @@
+<?php
+
+class Matematika {
+
+    public static function kali($a, $b) {
+        return $a * $b;
+    }
+
+    public static function bagi($a, $b) {
+        return $a / $b;
+    }
+
+}
+
+echo Matematika::kali(4, 5);
+echo "<br>";
+echo Matematika::bagi(10, 2);
+
+?>
